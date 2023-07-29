442e9167ae0b3dd7f53b5d290d86bb56a21e4e9f
Akira <griffcho16@gmail.com>
2023-07-29 09:33:26 +0900

    Initial Commit

---
+++ Bid_iOS/Bid_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bid_iOSApp.swift
+//  Bid_iOS
+//
+//  Created by Akira Cho on 2023/07/29.
+//
+
+import SwiftUI
+
+@main
+struct Bid_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bid_iOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Bid_iOS
+//
+//  Created by Akira Cho on 2023/07/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
